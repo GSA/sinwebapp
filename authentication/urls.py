06c9d4077dae9c0a8116aff5995ad20e486793de
chinchalinchin <chinchalinchin@gmail.com>
2020-06-19 08:05:56 -0400

    [BRMP-3414] landing page for oauth redirect uri

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login_page, name='login'),
-    path('logout/', views.logout_page, name='logout')
+    path('logout/', views.logout_page, name='logout'),
+    path('success/', views.logut_success, name='success')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 def login_page(request):
     return render(request, 'login_splash.html')
 
+def login_success(request):
+    return render(request, 'login_success.html')
+    
 def logout_page(request):
     return render(request, 'logout_splash.html')
+
