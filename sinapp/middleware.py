0cbecc893d958d5c64bc92b2fab397525e43b3bd
chinchalinchin <chinchalinchin@gmail.com>
2020-06-20 10:28:17 -0400

    adding library for jwt token decoding, updating middleware to intercept oauth callback and exchange code for token

--- sinapp/middleware.py
+++ sinapp/middleware.py
@@ -1,18 +1,42 @@
+from urllib.parse import urlencode
+from urllib.request import urlopen
+import jwt
 from django.http.request import HttpRequest
 from . import settings
 
 class UserInfoMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.token_exchange_url = 'https://uaa.fr.cloud.gov/oauth/token'
 
     def __call__(self, request: HttpRequest):
-
-        # code goes here
         path=request.path
-        code=request.GET.get('code',"nothing")
+
+        # DEBUG output
         if settings.DEBUG:
-            print('request path', path)
-            print('oauth code', code)
+            print('Intercepted Incoming Request Path:', path)
+            if path == '/auth/callback':
+                print('Detected OAuth Callback...')
+                print('OAuth CallBack Code Parameter:', request.GET.get('code',"nothing"))
+
+        # Retrieve User Info From Token
+        if path == '/auth/callback':
+            code = request.GET.get('code')
+            params = urlencode({
+                'code': code, 
+                'grant_type': 'authorization_code',
+                'response_type': 'token',
+                'client_id': settings.UAA_CLIENT_ID,
+                'client_secret': settings.UAA_CLIENT_SECRET,
+
+            })
+            post = urlopen(self.token_exchange_url, params)
+            post_response = post.read()
+            print(post_response)
+
+            # exchange code for token
+            # decode token
+            # save user info in session 
 
         response = self.get_response(request)
 
