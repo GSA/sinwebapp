29a44509aa493639ffd653030c5067995297c1ac
chinchalinchin <chinchalinchin@gmail.com>
2020-06-19 21:50:07 -0400

    add roles to model schema

--- authentication/models.py
+++ authentication/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    user_name: models.CharField(max_length=100)
-    user_email: models.CharField(max_length=200)
-    user_role: models.CharField(max_length=50)
+    user_name: models.CharField(max_length=50)
+    user_email: models.CharField(max_length=100)
+    role_id: models.ForeignKey(Role, on_delete=models.CASCADE)
+
+class Role(model.Models):
+    role_name: models.CharField(max_length=50)
