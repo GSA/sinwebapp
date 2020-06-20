d50cd0751d9d9758b39a522ea54a32373088bcbf
chinchalinchin <chinchalinchin@gmail.com>
2020-06-20 12:54:39 -0400

    pulled user email from database, print in template

--- sinwebapp/authentication/views.py
+++ sinwebapp/authentication/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from authentication.info_decoder import decodeUserInfo
+from authentication.user_info import retrieveUserEmail
 
 def login_page(request):
     return render(request, 'login_splash.html')
 
 @login_required
 def login_success(request):
-    decodeUserInfo(request)
-    return render(request, 'login_success.html')
+    user_email = retrieveUserEmail(request)
+    return render(request, 'login_success.html', {'user_email': user_email})
     
 def logout_page(request):
     return render(request, 'logout_splash.html')
-
