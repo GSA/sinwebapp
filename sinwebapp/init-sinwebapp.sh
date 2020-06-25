bd034bf5cdf4650cb5dd75e1e9b43887030ce9d9
chinchalinchin <chinchalinchin@gmail.com>
2020-06-25 07:52:35 -0400

    migrating database files in init script

--- sinwebapp/init-sinwebapp.sh
+++ sinwebapp/init-sinwebapp.sh
@@ -1,9 +1,16 @@
+echo "> Migrating Django Database Files..."
+
+python manage.py makemigrations
+python manage.py migrate 
+
 if [ "$1" == "local" ]
 then 
     python manage.py collectstatic --noinput
-    echo "> Binding Server To Non-Loopback Address for Local Configuration..."
+    echo ">> LOCAL Environment Detected"
+    echo ">>  Binding Server To Non-Loopback Address for Local Configuration..."
     gunicorn core.wsgi:application --bind=0.0.0.0
 else
-    echo "> Deploying Server Onto the Cloud..."
+    echo ">> CLOUD Environment Detected"
+    echo ">> Deploying Server Onto the Cloud..."
     gunicorn core.wsgi:application 
 fi
