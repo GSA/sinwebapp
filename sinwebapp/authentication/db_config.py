65f84d45070da3a8fefb3378aa6135fc640b7b0d
chinchalinchin <chinchalinchin@gmail.com>
2020-06-25 08:23:57 -0400

    adding wiring for migrating custom users, groups and permissions

---
+++ sinwebapp/authentication/db_config.py
@@ -0,0 +1,18 @@
+
+from django.contrib.auth.models import Group, Users, Permissions
+
+def init_groups():
+
+    # create groups here
+    pass
+
+def init_permissions():
+
+    # create permissions here
+    pass
+
+def init_users():
+
+    # create users here
+    pass
+    
