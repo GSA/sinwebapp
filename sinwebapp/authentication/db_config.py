27831591d2ba72e2098db90a402a085d9b5b593e
Teddy Desta <theodros.desta@gsa.gov>
2020-06-29 18:22:51 -0400

    [BRMP-3337] SIN Initial Permissions and users

--- sinwebapp/authentication/db_config.py
+++ sinwebapp/authentication/db_config.py
@@ -1,40 +1,41 @@
 from django.contrib.auth.models import Group, User, Permission
 from django.contrib.contenttypes.models import ContentType
 
-import logging
-
-logger = logging.getLogger(__name__)
-
 def init_groups(apps, schema_editor):
     # admin_user
-    admin_user, created = Group.objects.get_or_create(name='admin_user')
-    if created:
-        logger.info('Admin Group created')
+    admin_user = Group.objects.get_or_create(name='admin_user')
 
     # read_only
-    read_only, created = Group.objects.get_or_create(name='read_only')
-    if created:
-        logger.info('read_only Group created')
+    read_only = Group.objects.get_or_create(name='read_only')
 
     #submitter
-    submitter, created = Group.objects.get_or_create(name='submitter')
-    if created:
-        logger.info('Submitter Group created')
+    submitter = Group.objects.get_or_create(name='submitter')
 
     #reviewer
-    reviewer, created = Group.objects.get_or_create(name='reviewer')
-    if created:
-        logger.info('Reviewer Group created')
+    reviewer = Group.objects.get_or_create(name='reviewer')
 
     #approver
-    approver, created = Group.objects.get_or_create(name='approver')
-    if created:
-        logger.info('Approver Group created')
+    approver = Group.objects.get_or_create(name='approver')
 
 def init_permissions(apps, schema_editor):
-    pass
+    ct = ContentType.objects.get_for_model(User)
+    # read_sin
+    read_sin = Permission.objects.get_or_create(name="Can read SIN", content_type=ct, codename="read_sin")
+
+    # edit_sin (for reviewers)
+    edit_sin = Permission.objects.get_or_create(name="Can edit SIN", content_type=ct, codename="edit_sin")
+
+    # approve_sin
+    approve_sin = Permission.objects.get_or_create(name="Can approve SIN", content_type=ct, codename="approve_sin")
+
 
 def init_users(apps, schema_editor):
 
-    # create users here
-    pass
+    # name,email,pass
+    read_only_user = User.objects.create_user('read_only_user', 'read_only_user@gsa.gov', 'read_only_user')
+
+    submitter_user = User.objects.create_user('submitter_user', 'submitter_user@gsa.gov', 'submitter_user')
+
+    reviewer_user = User.objects.create_user('reviewer_user', 'reviewer_user@gsa.gov', 'reviewer_user')
+
+    approver_user = User.objects.create_user('approver_user', 'approver_user@gsa.gov', 'approver_user')
