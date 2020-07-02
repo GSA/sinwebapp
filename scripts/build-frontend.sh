37a65b58778bf1f330c1ae078520b744691fb78e
chinchalinchin <chinchalinchin@gmail.com>
2020-07-02 08:09:09 -0400

    adding django restframework to backend deps, configuring angular output folder, changing frontend build script to disregard output hashing

--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -1,13 +1,20 @@
-echo pwd
+echo "Present working directory: $(pwd)"
+
 if [[ $(pwd) =~ "scripts" ]]
 then
-    echo '> In scripts directory, moving to frontend directory...'
+    echo '> In /sinwebapp/scripts/ directory, moving to /sinwebapp/frontend/ directory...'
     cd ../frontend
 elif [[ $(pwd) =~ "sinwebapp" ]]
 then
-    echo '> In root directory, moving to frontend directory...'
+    echo '> In root /sinwebapp/ directory, moving to /sinwebapp/frontend/ directory...'
     cd frontend
 fi
 
+if [ -d '../sinwebapp/static/frontend' ]
+then
+    echo '> Cleaning /sinwebapp/static/frontend folder...'
+    rm -r ../sinwebapp/static/frontend/*
+fi
+
 echo "> Building Angular frontend..."
-ng build --prod
+ng build --prod --output-hashing none
