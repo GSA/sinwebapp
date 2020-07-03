75b23631b47dc58833bcf887a76cddfa64681c7b
chinchalinchin <chinchalinchin@gmail.com>
2020-07-03 04:55:31 -0400

    formatting test api response to meet angular request format

--- frontend/src/app/services/context.service.ts
+++ frontend/src/app/services/context.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 
+const LOCAL: number = 0
+const CLOUD: number = 1
+
 @Injectable({
   providedIn: 'root'
 })
--- sinwebapp/api/views.py
+++ sinwebapp/api/views.py
@@ -3,8 +3,9 @@ from django.http import JsonResponse
 def user_info(request):
     if hasattr(request.user, 'email'):
         response = JsonResponse({
-            'email': request.user.email
-        })
+            'email': request.user.email,
+            'name': 'Ted Danson'
+        }) 
     else:
         response = JsonResponse({
             'error': "No user signed in"
