71c852978abc2058a11c0f4ab60cef1e431ecf88
chinchalinchin <chinchalinchin@gmail.com>
2020-07-08 09:21:05 -0400

    removing circleci from this branch

--- sinwebapp/authentication/admin.py
+++ sinwebapp/authentication/admin.py
@@ -1,19 +1,2 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .db_config import GroupAdminForm 
-from debug import DebugLogger
-
-logger = DebugLogger("admin.py").get_logger()
-
-# Create a new Group admin.
-@admin.register(Group)
-class GroupAdmin(admin.ModelAdmin):
-    # Use our custom form.
-    form = GroupAdminForm
-    # Filter permissions horizontal as well.
-    filter_horizontal = ['permissions']
-    
-# Unregister the original Group admin.
-# admin.site.unregister(Group)
-# Register the new Group ModelAdmin.
-# admin.site.register(Group, GroupAdmin)
