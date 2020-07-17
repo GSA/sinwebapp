9f451eb1236230f9f11ec82af950b10c0c47c729
chinchalinchin <chinchalinchin@gmail.com>
2020-07-17 06:10:27 -0400

    [BRMP-4372] adding deploy job to circleci config and making script for it to invoke

--- scripts/cc-cf-push.sh
+++ scripts/cc-cf-push.sh
@@ -1,2 +1,25 @@
-### Note: $CIRCLECI_DEV_USER, $CIRCLECI_DEV_PASSWORD store the CloudFoundry login
-### credentials on the CircleCi environment. 
+### ARGUMENTS
+# $1: Required: dev, staging or prod
+
+### Note: $CF_DEV_USER, $CF_DEV_PASSWORD store the CloudFoundry login
+### credentials on the CircleCi environment. $CF_ORGANIZATION, 
+### $CF_DEV_SPACE, $CF_STAGING_SPACE and $CF_PROD_SPACE store the
+### attributes used to target an organization and space on 
+### CloudFounry.
+
+# TODO: verify number of arguments, else exit
+
+if [ "$1" == "dev" ]
+then
+    cf auth $CF_DEV_USERNAME $CF_DEV_PASSWORD
+    cf target -o $CF_ORGANIZATION -s $CF_DEV_SPACE
+elif [ "$1" == "prod" ]
+then
+    cf auth $CF_DEV_USERNAME $CF_DEV_PASSWORD
+    cf target -o $CF_ORGANIZATION -s $CF_PROD_SPACE
+elif [ "$1" == "staging" ]
+then
+    cf auth $CF_DEV_USERNAME $CF_DEV_PASSWORD
+    cf target -o $CF_ORGANIZATION -s $CF_STAGING_SPACE
+fi
+
