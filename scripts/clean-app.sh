e8d133a23bdd590c87f9a50bcbcb51de00a84382
chinchalinchin <chinchalinchin@gmail.com>
2020-07-17 07:49:39 -0400

    [BRMP-4372] installing wget, correct workspace error

--- scripts/clean-app.sh
+++ scripts/clean-app.sh
@@ -10,8 +10,21 @@ then
     formatted_print '--> Cleaning \e[4m/static/\e[0m Directory' $SCRIPT_NAME
     rm -r $SCRIPT_DIR/../sinwebapp/static/
 fi
+
 if [ -d "$SCRIPT_DIR/../frontend/node_modules/" ]
 then
     formatted_print '--> Cleaning \e[4m/node_modules/\e[0m Directory' $SCRIPT_NAME
     rm -r $SCRIPT_DIR/../frontend/node_modules/
+fi
+
+if [ -d "$SCRIPT_DIR/../sinwebapp/util/" ]
+then
+    formatted_print '--> Cleaning \e[4m/sinwebapp/util\e[0m Directory' $SCRIPT_NAME
+    rm -r $SCRIPT_DIR/../sinwebapp/init-app.sh
+fi
+
+if [ -f "$SCRIPT_DIR/../sinwebapp/init-app.sh" ]
+then 
+    formatted_print '--> Cleaning \e[4m/sinwebapp/init-app.sh\e[0m File' $SCRIPT_NAME
+    rm $SCRIPT_DIR/../sinwebapp/init-app.sh
 fi
