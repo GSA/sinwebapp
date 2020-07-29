c5898907390b22670829e948b705d1307f084ee7
chinchalinchin <chinchalinchin@gmail.com>
2020-07-29 12:51:36 -0400

    [BRMP-4619] change status field from int to char

--- sinwebapp/api/models.py
+++ sinwebapp/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Status(models.Model):
-    status = models.IntegerField(null=False)
+    status = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
 
 class Sin(models.Model):
