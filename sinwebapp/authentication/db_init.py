422cde50a9e4b29e39aca9f1a436dcd73b6f592d
chinchalinchin <chinchalinchin@gmail.com>
2020-07-29 11:46:42 -0400

    [BRMP-4619] reorganize auth groups to reflect changes in model

--- sinwebapp/authentication/db_init.py
+++ sinwebapp/authentication/db_init.py
@@ -7,8 +7,7 @@ def init_groups(apps, schema_editor):
     logger = DebugLogger("authentication.db_config.init_groups").get_logger()
     logger.info("> Initializing Groups...")
 
-    admin_user_group = Group.objects.get_or_create(name='admin_user_group')
-    read_only_group = Group.objects.get_or_create(name='read_only_group')
+    admin_group = Group.objects.get_or_create(name='admin_group')
     submitter_group = Group.objects.get_or_create(name='submitter_group')
     reviewer_group = Group.objects.get_or_create(name='reviewer_group')
     approver_group = Group.objects.get_or_create(name='approver_group')
@@ -18,9 +17,6 @@ def init_permissions(apps, schema_editor):
     logger.info("> Initializing Permissions...")
 
     ct = ContentType.objects.get_for_model(User)
-
-    admin_sin = Permission.objects.get_or_create(name='Administer SIN', content_type=ct, codename='admin_sin')
-    read_sin = Permission.objects.get_or_create(name='Read SIN', content_type=ct, codename='read_sin')
     submit_sin = Permission.objects.get_or_create(name='Submit SIN', content_type=ct, codename='submit_sin')
     review_sin = Permission.objects.get_or_create(name='Review SIN', content_type=ct, codename='review_sin')
     approve_sin = Permission.objects.get_or_create(name='Approve SIN', content_type=ct, codename='approve_sin')
@@ -28,11 +24,7 @@ def init_permissions(apps, schema_editor):
 def init_group_permissions(apps, schema_editor):
     logger = DebugLogger("authentication.db_config.init_group_permissions").get_logger()
     logger.info("> Initializing Group Permissions...")
-
-    read_only_group = Group.objects.get(name='read_only_group')
-    read_only_permissions = Permission.objects.get(codename='read_sin')
-    read_only_group.permissions.add(read_only_permissions)
-
+    
     submitter_group = Group.objects.get(name='submitter_group')
     submitter_permissions = Permission.objects.get(codename='submit_sin')
     submitter_group.permissions.add(submitter_permissions)
@@ -44,3 +36,8 @@ def init_group_permissions(apps, schema_editor):
     approver_group = Group.objects.get(name='approver_group')
     approver_permissions = Permission.objects.get(codename='approve_sin')
     approver_group.permissions.add(approver_permissions)
+
+    admin_group = Group.objects.get(name='admin_group')
+    admin_group.permissions.add(submitter_permissions)
+    admin_group.permissions.add(reviewer_group)
+    admin_group.permissions.add(approver_group)
