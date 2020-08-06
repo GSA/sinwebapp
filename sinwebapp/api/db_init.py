6d19534551d6785becc01c43318b8de767687abf
chinchalinchin <chinchalinchin@gmail.com>
2020-08-06 17:29:30 -0400

    [BRMP-4425] ensure status response maps to angular model

--- sinwebapp/api/db_init.py
+++ sinwebapp/api/db_init.py
@@ -4,15 +4,15 @@ from api.models import Status
 def init_status(apps, schema_editor):
     logger = DebugLogger("authentication.db_config.init_groups").get_logger()
 
-    submitted = Status.objects.get_or_create(status='submitted', 
+    submitted = Status.objects.get_or_create(name='submitted', 
                                              description="SIN form has been submitted for review and approval.")
-    reviewed = Status.objects.get_or_create(status='reviewed', 
+    reviewed = Status.objects.get_or_create(name='reviewed', 
                                              description="SIN form has been reviewed and is ready for approval.")
-    change = Status.objects.get_or_create(status='change', 
+    change = Status.objects.get_or_create(name='change', 
                                              description="SIN form requires change before approval.")
-    approved = Status.objects.get_or_create(status='approved', 
+    approved = Status.objects.get_or_create(name='approved', 
                                             description="SIN form has been approved.")
-    denied = Status.objects.get_or_create(status='denied', 
+    denied = Status.objects.get_or_create(name='denied', 
                                              description="SIN form has been denied.")
-    expired = Status.objects.get_or_create(status='expired',
+    expired = Status.objects.get_or_create(name='expired',
                                              description="SIN form has expired.")
