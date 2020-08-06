86ed596818df317e0cd8b6d970abde5c74fc6377
chinchalinchin <chinchalinchin@gmail.com>
2020-08-06 11:20:45 -0400

    [BRMP-4425] adding output event emitters to let sin selection traverse angular components

--- frontend/src/app/components/submit-display/submit-display.component.ts
+++ frontend/src/app/components/submit-display/submit-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { SinService } from 'src/app/services/sin.service';
 import { SIN, null_SIN } from 'src/app/models/sin';
 import { LogService } from 'src/app/services/log.service';
@@ -19,7 +19,10 @@ export class SubmitDisplayComponent implements OnInit {
   public submit_SIN : SIN = null_SIN;
   public selected_SIN: SIN = null_SIN;
   public user_SINs: SIN[] =[];
-  @Input() user: User;
+  @Input() 
+  public user: User;
+  @Output() 
+  public selection_event = new EventEmitter<SIN>();
 
 
   constructor(private sin: SinService,
@@ -63,6 +66,7 @@ export class SubmitDisplayComponent implements OnInit {
   public selectSIN(sin: SIN){
     this.logger.log(`Selecting SIN: # ${sin.sin_number}`, `${this.class_name}.selectSIN`)
     this.selected_SIN = sin;
+    this.selection_event.emit(sin);
   }
   
 }
--- frontend/src/app/components/user-display/user-display.component.ts
+++ frontend/src/app/components/user-display/user-display.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User, null_User } from '../../models/user';
 import { UserService } from '../../services/user.service'
 import { LogService } from 'src/app/services/log.service';
+import { null_SIN, SIN } from 'src/app/models/sin';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-user-display',
@@ -12,6 +14,7 @@ export class UserDisplayComponent implements OnInit {
   private class_name = "UserDisplayComponent";
 
   public user : User = null_User
+  public selected_SIN: SIN = null_SIN;
 
   constructor(private userService: UserService,
                 private logger: LogService) { }
@@ -21,10 +24,16 @@ export class UserDisplayComponent implements OnInit {
     this.getUser()
   }
 
-  getUser(): void {
+  public getUser(): void {
+    this.logger.log('Retrieving User', `${this.class_name}.getUser`)
     this.userService.getUser().subscribe((user : User) =>{
       this.logger.log('User Retrieved', `${this.class_name}.getUser`)
       this.user = user;
     })
+  }
+
+  public selectSIN(sin: SIN): void{
+    this.logger.log(`Selecting SIN: #${sin.sin_number}`, `${this.class_name}.getUser`)
+    this.selected_SIN = sin;
   }
 }
