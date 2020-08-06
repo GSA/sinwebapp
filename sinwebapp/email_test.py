88b4e68e25491e163b93feaa4999ada5bf7cfe9f
chinchalinchin <chinchalinchin@gmail.com>
2020-08-06 08:14:44 -0400

    testing out email service

--- sinwebapp/email_test.py
+++ sinwebapp/email_test.py
@@ -1,26 +1,31 @@
-import smtplib
+import smtplib, sys
 from debug import DebugLogger
 
-logger = DebugLogger("email_test").getlogger()
+logger = DebugLogger("email_test").get_logger()
 
 
-send_user = "sinweb@cloud.gov" 
-#gmail_password = 
-receive_user = ['grant.moore@gsa.gov']
+send_user = "grant.moore@gsa.gov" 
+#send_user_password = 
+receive_user = ['grant.moore@gsa.gov', 'chinchalinchin@gmail.com']
 message = "testing testing 123"
 
 logger.info("Testing email from %s to %s", send_user, receive_user)
 try:
     logger.info("Opening SMTP connection to smtp.gsa.gov")
-    server = smtplib.SMTP("smtp.gsa.gov")
+    server = smtplib.SMTP(host='smtp.gsa.gov', port=25, timeout=30)
+    server.ehlo()
         #server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        #server.ehlo()
-        #server.login(gmail_user, gmail_password)
+        #server.login(send_user, send_user_password)
     logger.info("Sending mail")
     server.sendmail(send_user, receive_user, message)
     logger.info("Closing SMTP connection to smtp.gsa.gov")
     server.close()
 
 except:
-    e = sys.exc_info()[0]
-    logger.warn("Unable To Send Email, Error: %s", e)
+    logger.warning("Unable To Send Email")
+    e1 = sys.exc_info()[0]
+    e2 = sys.exc_info()[1]
+    e3 = sys.exc_info()[2]
+    logger.warning("Error Type: %s", e1)
+    logger.warning("Error Value: %s", e2)
+    logger.warning("Error Stack: %s", e3)
