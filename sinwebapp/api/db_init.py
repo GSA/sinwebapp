899f55e1bf8bccac99f9b438fb42ef7cc0c99e6f
chinchalinchin <chinchalinchin@gmail.com>
2020-08-24 15:52:59 -0400

    [BRMP-4911] upload csv

--- sinwebapp/api/db_init.py
+++ sinwebapp/api/db_init.py
@@ -28,38 +28,26 @@ def init_sindata(app, schema_editor):
     logger = DebugLogger("api.db_init.init_sindata").get_logger()
     logger.info("Initializing Sin data")
 
-#     Sin = ContentType.objects.get_for_model(Sin)
-#     sin_number = Sin.objects.get_or_create(name=row[0], content_type=sin, codename='sin_number')
-#     schedule_number = Sin.objects.get_or_create(name=row[1], content_type=sin, codename='schedule_number')
-#     special_item_number = Sin.objects.get_or_create(name=row[2], content_type=sin, codename='special_item_number')
-#     sin_group_title = Sin.objects.get_or_create(name=row[3], content_type=sin, codename='sin_group_title')
-#     sin_description1 = Sin.objects.get_or_create(name=row[4], content_type=sin, codename='sin_description1')
-#     sin_description2 = Sin.objects.get_or_create(name=row[5], content_type=sin, codename='sin_description2')
-#     sin_order = Sin.objects.get_or_create(name=row[6], content_type=sin, codename='sin_order')
-#     co_fname = Sin.objects.get_or_create(name=row[7], content_type=sin, codename='co_fname')
-#     co_lname = Sin.objects.get_or_create(name=row[8], content_type=sin, codename='co_lname')
-#     co_phone = Sin.objects.get_or_create(name=row[9], content_type=sin, codename='co_phone')
-#     co_email = Sin.objects.get_or_create(name=row[10], content_type=sin, codename='co_email')
-
-
     # import data loop thru the objects
     # Skip first row
     # def import_data():
     with open('../db/sin_data.csv') as f:
         reader = csv.reader(f)
+        next(reader)
+        count=0
         for row in reader:
-            next(reader, None) 
             created = SinData.objects.get_or_create(
-                  sin_number=row[0],
-                  schedule_number=row[1],
-                  special_item_number=row[2],
-                  sin_group_title=row[3],
-                  sin_description1=row[4],
-                  sin_description2=row[5],
-                  sin_order=row[6],
-                  co_fname=row[7],
-                  co_lname=row[8],
-                  co_phone=row[9],
-                  co_email=row[10]
-                )
-            logger.info("Sin Created")
+                sin_number=row[0],
+                schedule_number=row[1],
+                special_item_number=row[2],
+                sin_group_title=row[3],
+                sin_description1=row[4],
+                sin_description2=row[5],
+                sin_order=row[6],
+                co_fname=row[7],
+                co_lname=row[8],
+                co_phone=row[9],
+                co_email=row[10]
+            )
+            count+=1
+            logger.info("Sin Created id # %s", count)
