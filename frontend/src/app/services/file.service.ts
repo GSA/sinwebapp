2f6da40cafb8d07ac267a4ba4c41ddf8b537c106
chinchalinchin <chinchalinchin@gmail.com>
2020-09-12 09:36:23 -0400

    [BRMP-5871] add file upload url to file service in frontend

--- frontend/src/app/services/file.service.ts
+++ frontend/src/app/services/file.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormBuilder, FormGroup, FormData } from '@angular/forms'
+import { FormGroup } from '@angular/forms'
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { LogService } from './log.service';
+import { ContextService } from './context.service';
 
 
 
@@ -15,17 +16,18 @@ export class FileService {
   private class_name : String = "FileService"
 
   constructor(private http: HttpClient,
+              private context: ContextService,
               private logger: LogService) { }
 
-  public uploadFile(form: FormGroup): Observable{
+  public uploadFile(form: FormGroup): Observable<any>{
     const formData = new FormData()
     formData.append('file', form.get('file').value)
     formData.append('sin_number', form.get('sin_number').value)
 
-    this.http.post<FormData>('url goes here', formData).pipe( 
+    return this.http.post<FormData>(this.context.getFileUploadUrl(), formData).pipe( 
       tap( () => { this.logger.log( "Posting SIN", `${this.class_name}.postSIN`);}),
       catchError(this.handleError('postSIN'))
-      );
+    );
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
