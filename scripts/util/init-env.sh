3171a33e766b2a4c2ba1286eaaa992d4a54dbec8
chinchalinchin <chinchalinchin@gmail.com>
2021-01-29 15:12:04 -0500

    correct directory structure

--- scripts/util/init-env.sh
+++ scripts/util/init-env.sh
@@ -10,10 +10,10 @@ if [ "$1" == "--help" ] || [ "$1" == "--h" ] || [ "$1" == "-help" ] || [ "$1" ==
 then
     help_print "$SCRIPT_DES" $SCRIPT_NAME
 else
-    if [ -f "$SCRIPT_DIR/../env/local.env" ]
+    if [ -f "$SCRIPT_DIR/../../env/local.env" ]
     then
         set -o allexport
-        source $SCRIPT_DIR/../env/local.env
+        source $SCRIPT_DIR/../../env/local.env
         set +o allexport
     else
         log 'Please Configure \e[4mlocal.env\e[0m File' $SCRIPT_NAME
